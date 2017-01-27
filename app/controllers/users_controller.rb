11a844baee197bcb6aecace3c82ea9e7e01c2353
Aaron Janke <aaronljanke@gmail.com>
2017-01-27 14:33:51 -0600

    dfsa

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_request, only: [:index, :create]
   wrap_parameters :user, include: [:email, :name, :password, :password_confirmation, :id, :password_digest]
-  before_action :add_header
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
 
   # GET /users
   def index
@@ -39,6 +40,27 @@ class UsersController < ApplicationController
   # DELETE /users/1
   def destroy
     @user.destroy
+  end
+
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+# If this is a preflight OPTIONS request, then short-circuit the
+# request, return only the necessary headers and return an empty
+# text/plain.
+
+  def cors_preflight_check
+    if request.method == :options
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
+      headers['Access-Control-Max-Age'] = '1728000'
+      render :text => '', :content_type => 'text/plain'
+    end
   end
 
   private
