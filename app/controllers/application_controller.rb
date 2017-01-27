1528fa61679b89fd2734a54d11edef600cf345c9
Aaron Janke <aaronljanke@gmail.com>
2017-01-27 15:07:04 -0600

    fdf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,6 @@
 class ApplicationController < ActionController::API
 	before_action :authenticate_request 
 	attr_reader :current_user
-	before_filter :allow_ajax_request_from_other_domains 
-	
-	def allow_ajax_request_from_other_domains
-   	  headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Request-Method'] = '*'
-      headers['Access-Control-Allow-Headers'] = '*'
-    end
 
 	private 
 	def authenticate_request 
