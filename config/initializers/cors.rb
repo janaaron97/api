cc8dd282bbefad21f311788107a29bfc7d78c57a
Aaron Janke <aaronljanke@gmail.com>
2017-01-27 14:05:27 -0600

    blank commit name

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins 'railsfrontend.herokuapp.com/'
+     origins 'http://railsfrontend.herokuapp.com/'
 
      resource '*',
        headers: :any,
