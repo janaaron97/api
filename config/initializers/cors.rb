d74e626c874e38cc3074dd71231d1c7cb4eb1b04
Aaron Janke <aaronljanke@gmail.com>
2017-01-27 13:56:55 -0600

    dfa

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins 'http://railsfrontend.herokuapp.com/'
+     origins 'https://railsfrontend.herokuapp.com/'
 
      resource '*',
        headers: :any,
