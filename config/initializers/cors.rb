5fb48c5a465460e6a7f2f4d6f886d6bf1e6a7017
Aaron Janke <aaronljanke@gmail.com>
2017-01-27 14:44:57 -0600

    dafsd

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins 'http://railsfrontend.herokuapp.com/'
+     origins '*'
 
      resource '*',
        headers: :any,
